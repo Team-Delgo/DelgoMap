332bad4099d5540ec94a1893abe785aa374d1318
gupuroom <cjsrnr1114@naver.com>
2023-02-01 15:19:37 +0900

    ADD : sitemap, meta tag

---
+++ src/sitemapRoutes.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import DetailPage from './page/DetailPage';
+import MapPage from './page/MapPage';
+
+function App() {
+    const queryClient = new QueryClient();
+
+
+    return <QueryClientProvider client={queryClient}><BrowserRouter>
+        <Routes>
+            <Route path='/' element={<MapPage />} />
+            <Route path='/:id' element={<MapPage />} />
+            <Route path='/detail' element={<DetailPage />} />
+            <Route path='/:id/detail' element={<DetailPage />} />
+        </Routes>
+    </BrowserRouter>
+    </QueryClientProvider>;
+}
+
+export default App;
