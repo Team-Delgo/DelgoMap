4e3f20cfe8c942862efc1aee0fc6851192005ed6
delgo <cjsrnr1114@naver.com>
2023-04-16 17:12:44 +0900

    FIX: certtoggle

--- src/page/map/components/CertToggle.tsx
+++ src/page/map/components/CertToggle.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import classNames from "classnames";
 import DogFoot from "../../../common/icons/dogfoot.svg";
 import "./CertToggle.scss";
 
 function CertToggle(props:{onClick:()=>void, state:boolean}){
   const {onClick, state} = props;
-  return <div className={classNames("certtoggle",{off:!state})} aria-hidden="true" onClick={onClick}>
+  const [isDisabled, setIsDisabled] = useState(false);
+  const onClickHandler = () => {
+    if(!isDisabled){
+      onClick();
+      setIsDisabled(true);
+      setTimeout(()=>setIsDisabled(false),100);
+    }
+  };
+  return <div className={classNames("certtoggle",{off:!state})} aria-hidden="true" onClick={onClickHandler}>
     <img src={DogFoot} alt="toggle"/>
   </div>
 };
