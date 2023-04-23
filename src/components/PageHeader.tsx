04f7458db684f45ce74e8423c92d14278468e2db
cksrb63 <cksr1@naver.com>
2023-04-23 10:42:39 +0900

    ADD : header layout

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PageHeader(props: {
+  title: string,
+  navigate: () => void,
+  isFixed: boolean,
+  isAbsolute: boolean,
+}) {
+  const { title, navigate, isFixed, isAbsolute } = props;
+  return <div />
+};
+
+export default PageHeader;
