1f517c105d1e9bdd5a71327e15c57f401ff38c36
ckrb63 <cksr1@naver.com>
2023-04-23 15:48:34 +0900

    ADD : help message toggle

--- src/components/HelpFloatingMessage.tsx
+++ src/components/HelpFloatingMessage.tsx
@@ -1,9 +1,14 @@
+import classNames from 'classnames';
 import React from 'react';
 import './HelpFloatingMessage.scss';
 
 function HelpFloatingMessage(props: { text: string; direction: string }) {
   const { text, direction } = props;
-  return <div className="help-floating">추억을 기록해보세요</div>;
+  return (
+    <div className={classNames('help-floating', { right: direction === 'right' })}>
+      {text}
+    </div>
+  );
 }
 
 export default HelpFloatingMessage;
