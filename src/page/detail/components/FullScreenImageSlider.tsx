4cc7e97f09d8f31cf4555656a0386e3b38f8ad62
ckrb63 <cksr1@naver.com>
2023-05-31 16:54:55 +0900

    ADD : detail page slider , data fetching

---
+++ src/page/detail/components/FullScreenImageSlider.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import Exit from '../../../common/icons/exit.svg';
+
+interface Props {
+  images: string[];
+  index: number;
+  placeName: string;
+  close: () => void;
+}
+
+function FullScreenImageSlider({ images, index, placeName, close }: Props) {
+  const [imageNumber, setImageNumber] = useState(index);
+
+  return (
+    <div className="detail-full">
+      <div className="detail-full-header">
+        {placeName}
+        <img src={Exit} alt="exit" aria-hidden onClick={close} />
+      </div>
+      <div className="detail-full-image">
+        <Swiper onSlideChange={(swiper) => setImageNumber(swiper.activeIndex)}>
+          {images.map((image) => {
+            return (
+              <SwiperSlide>
+                <img src={image} alt="placeimage" />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </div>
+  );
+}
+
+export default FullScreenImageSlider;
