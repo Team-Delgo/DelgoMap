59005ba05035f65d8da577d7daafd7b63865a5fb
jungseokwoo <wjdtjrdn6@gmail.com>
2023-07-16 15:38:50 +0900

    config: babel jest 설정파일 추가

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(react-merge-refs|react-modal-sheet)/)",
+  ],
+  testEnvironment: 'jsdom',
+  setupFiles: ['<rootDir>/__mocks__/localStorageMock.js'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
