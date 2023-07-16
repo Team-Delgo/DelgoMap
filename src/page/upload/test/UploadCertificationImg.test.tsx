aa66a5a0809a2c81f55117dc957ebeccc30eeb67
jungseokwoo <wjdtjrdn6@gmail.com>
2023-07-16 15:39:13 +0900

    test: 인증 이미지 테스트 코드 추가

---
+++ src/page/upload/test/UploadCertificationImg.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import store from '../../../redux/store';
+import UploadCertificationImg from '../components/UploadCertificationImg';
+
+const mockOpenBottomSheet = jest.fn();
+
+describe('UploadCertificationImg', () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <UploadCertificationImg openBottomSheet={mockOpenBottomSheet} />
+        </MemoryRouter>
+      </Provider>,
+    );
+  });
+
+  it('renders without crashing', () => {
+    expect(screen.getByText('사진 추가')).toBeInTheDocument();
+  });
+
+  it("calls openBottomSheet when 'x' icon is clicked", () => {
+    fireEvent.click(screen.getByAltText('capture-page-x'));
+    expect(mockOpenBottomSheet).toHaveBeenCalled();
+  });
+});
