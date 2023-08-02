2edfd5eac19f90874473d62b5a8ee7e6a2ab804f
yyunee <gimyunhe@gmail.com>
2023-08-02 15:40:24 +0900

    [FIX]signup-term, SignupComplete(+userinfo,phone) migration

--- src/page/sign/signup/term/DetailTerm.tsx
+++ src/page/sign/signup/term/DetailTerm.tsx
@@ -7,9 +7,13 @@ function DetailTerm(props: { id: number }) {
   const keyTyped = id as keyof typeof term;
 
   return (
-    <div className="wrapper">
-      <p className="title">{term[keyTyped].main}</p>
-      <p className="description">{term[keyTyped].description}</p>
+    <div className="mx-[34px] my-[46px] max-h-[60vh] overflow-scroll">
+      <p className="w-[63vw] text-[16px] font-bold leading-[150%] text-[#3f3f3f]">
+        {term[keyTyped].main}
+      </p>
+      <p className="leading[150%] mt-[25px] whitespace-pre-line text-xs font-normal text-[#3f3f3f]">
+        {term[keyTyped].description}
+      </p>
     </div>
   );
 }
