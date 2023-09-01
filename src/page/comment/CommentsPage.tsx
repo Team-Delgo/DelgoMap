2eaaec4da97d5c91dc1e71b7c8ff2040c1027b1e
yyunee <gimyunhe@gmail.com>
2023-09-01 17:40:06 +0900

    [FIX]record 로그인 잠금해제

--- src/page/comment/CommentsPage.tsx
+++ src/page/comment/CommentsPage.tsx
@@ -41,6 +41,7 @@ function CommentsPage() {
   const { post } = useLocation()?.state as StateType;
   const textRef = useRef<any>(null); //textarea 높이설정을 위한 ref객체
   const navigate = useNavigate();
+  const location = useLocation();
   const dispatch = useDispatch();
   const commentEvent = useAnalyticsCustomLogEvent(analytics, 'cert_comment_post');
 
@@ -139,16 +140,16 @@ function CommentsPage() {
     }
   }, []);
 
-  // const profileClickHandler = (commentId: number) => {
-  //   if (commentId) {
-  //     console.log(commentId);
-  //     navigate(`${RECORD_PATH.PHOTO}/${commentId}`, {
-  //       state: {
-  //         prevPath: location.pathname,
-  //       },
-  //     });
-  //   }
-  // };
+  const profileClickHandler = (commentId: number) => {
+    if (commentId) {
+      console.log(commentId);
+      navigate(`${RECORD_PATH.PHOTO}/${commentId}`, {
+        state: {
+          prevPath: location.pathname,
+        },
+      });
+    }
+  };
 
   //댓글삭제 바텀시트 오픈 (삭제한 댓글id 저장)
   const openBottomSheet = useCallback(
@@ -172,7 +173,7 @@ function CommentsPage() {
         <img
           src={comment.userProfile}
           alt="profile"
-          // onClick={() => profileClickHandler(comment.userId)}
+          onClick={() => profileClickHandler(comment.userId)}
         />
         <div className="comment-content">
           <div className="comment-content-header">
