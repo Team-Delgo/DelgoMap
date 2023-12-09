ef577836271bb336a39fa87cf25e16fe42c49d87
FE-SW <ingal777@naver.com>
2023-12-09 21:42:58 +0900

    feat: 공통 인터페이스, 오브젝트 추가

--- src/common/types/mungPlace.ts
+++ src/common/types/mungPlace.ts
@@ -1,13 +1,19 @@
 export interface MungPlaceType {
-    categoryCode: string;
-    geoCode: string;
-    jibunAddress: string;
-    latitude: string;
-    longitude: string;
-    mungpleId: number;
-    p_geoCode: string;
-    placeName: string;
-    registDt: string;
-    address: string;
-  }
-  
+  categoryCode: string;
+  geoCode: string;
+  jibunAddress: string;
+  latitude: string;
+  longitude: string;
+  mungpleId: number;
+  p_geoCode: string;
+  placeName: string;
+  registDt: string;
+  address: string;
+}
+
+export interface MostVisitedPlace {
+  mungpleId: number;
+  photoUrl: string;
+  placeName: string;
+  visitCount: number;
+}
