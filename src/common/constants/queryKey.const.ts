82f12eff38a14baa02da90b65edefde7e3c4fb66
FE-SW <ingal777@naver.com>
2023-12-09 21:42:45 +0900

    feat: getMyUserInfo 쿼리키 추가

--- src/common/constants/queryKey.const.ts
+++ src/common/constants/queryKey.const.ts
@@ -9,5 +9,6 @@ export const GET_PET_TYPE_DATA_LIST = 'getPetTypeDataList';
 export const GET_ALL_CERTIFICATION_DATA_LIST = "getAllCertificationPostsList";
 export const GET_MY_PROFILE_INFO_DATA = "getMyProfileInfoData";
 export const GET_ACHIEVEMENT_DATA_LIST = 'getAchievementDataList'
+export const GET_MY_USER_INFO = "getMyUserInfo"
 export const CACHE_TIME = 1000 * 60 * 5;
 export const STALE_TIME = 1000 * 60 * 3;
